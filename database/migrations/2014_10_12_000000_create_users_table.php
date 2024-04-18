c8f1bd8b0d8f488fb0bf24e0223993e7a17f0485
Adam Lopez <adamf04@gmail.com>
2024-04-18 13:02:57 -0400

    Installing laravel breeze package for login and altering the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('is_admin')->default("false"); // boolean field, but stored as string in db
             $table->string('password');
+            $table->string('ss_number', 9);
             $table->rememberToken();
             $table->timestamps();
         });
